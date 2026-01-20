91910fd6273d15489fab882467bae427fb8033ee
Wurth <fortezagerman@gmail.com>
2026-01-20 11:06:16 -0300

    Update app_mundial.py

--- app_mundial.py
+++ app_mundial.py
@@ -239,7 +239,7 @@ if datos_cargados:
         st.markdown("<div style='text-align: center; margin-top: 50px;'>", unsafe_allow_html=True)
         st.markdown("## 📅 PARTIDOS Y PUNTOS DEL CAMPEONATO")
         st.markdown(f"""
-            <a href="#" target="_blank" style="text-decoration: none;">
+            <a href="https://viewer.ipaper.io/wurth-uruguay/world-cup/wurth-world-cup-2026/" target="_blank" style="text-decoration: none;">
                 <div style='display: inline-block; padding: 20px 50px; background-color: #cc0000; border-radius: 50px; border: 2px solid white; margin-top:30px;'>
                     <span style='color: white !important; font-family: "WuerthExtra"; font-size: 24px;'>VER INFORMACIÓN 📊</span>
                 </div>
